734af94b723803e3a0d90d9091871262a86e14c8
polbebe <pb2750@columbia.edu>
2020-07-09 13:10:42 -0400

    Camera & Sense HAT added

---
+++ Camera/camera_test.py
@@ -0,0 +1,15 @@
+# Never save a file as picamera.py
+
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+camera.rotation = 180
+
+camera.start_preview(alpha=230)
+
+#for i in range(3):
+#    sleep(5)
+#    camera.capture('/home/pi/Desktop/image{}.jpg'.format(i))
+sleep(5)
+camera.stop_preview()

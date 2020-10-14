2d8e8fd457d7209462b44b3f1d248e6fa4ae6150
polbebe <pb2750@columbia.edu>
2020-10-14 10:04:36 -0400

    v2 bug fixess

--- Networks/v2/PP_client.py
+++ Networks/v2/PP_client.py
@@ -34,15 +34,15 @@ s41 = LX16A(41)
 
 # Initialize socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-	# Connect to network
-	s.connect((HOST, PORT))
+    # Connect to network
+    s.connect((HOST, PORT))
 
-	# Motor Inputs array
-	motor_inputs = np.zeros(12, dtype=np.float32)
+    # Motor Inputs array
+    motor_inputs = np.zeros(12, dtype=np.float32)
 
-	# Start walking
-	while True:
-		# Calculate motor_inputs
+    # Start walking
+    while True:
+        # Calculate motor_inputs
         #motor_inputs[0] = 0
         motor_inputs[1] = s12.getPhysicalPos() #- x.getVirtualPos()
         motor_inputs[2] = s11.getPhysicalPos() #- x.getVirtualPos()
@@ -56,38 +56,38 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         motor_inputs[10] = s42.getPhysicalPos() #- x.getVirtualPos()
         motor_inputs[11] = s41.getPhysicalPos() #- x.getVirtualPos()
         # Send motor inputs to server
-		s.sendall(motor_inputs.tobytes())
-		
-		# Get position data from server
-		p = s.recv(1024)
-		pos = np.frombuffer(p, dtype=np.float32)
-		# If there's no more data break the loop
-		if not pos:
-			break
-		# Move all servos to their corresponding position
-		s10.moveTimeWrite(130)
-		# LEFT FRONT
-		# Move servo 12 (elbow)
-		s12.moveTimeWrite(pos[1]) #print(pos[1])
-		# Move servo 11 (knee)
-		s11.moveTimeWrite(pos[2]) #print(pos[2])
-		# LEFT BACK
-		# Move servo 32 (elbow)
-		s32.moveTimeWrite(pos[7]) #print(pos[7])
-		# Move servo 31 (knee)
-		s31.moveTimeWrite(pos[8]) #print(pos[8])
-		# RIGHT FRONT
-		# Move servo 22 (elbow)
-		s22.moveTimeWrite(pos[4]) #print(pos[4])
-		# Move servo 21 (knee)
-		s21.moveTimeWrite(pos[5]) #print(pos[5])
-		# RIGHT BACK
-		# Move servo 42 (elbow)
-		s42.moveTimeWrite(pos[10]) #print(pos[10])
-		# Move servo 41 (knee)
-		s41.moveTimeWrite(pos[11]) #print(pos[11])
+        s.sendall(motor_inputs.tobytes())
+        
+        # Get position data from server
+        p = s.recv(1024)
+        pos = np.frombuffer(p, dtype=np.float32)
+        # If there's no more data break the loop
+        if not p:
+            break
+        # Move all servos to their corresponding position
+        s10.moveTimeWrite(130)
+        # LEFT FRONT
+        # Move servo 12 (elbow)
+        s12.moveTimeWrite(pos[1]) #print(pos[1])
+        # Move servo 11 (knee)
+        s11.moveTimeWrite(pos[2]) #print(pos[2])
+        # LEFT BACK
+        # Move servo 32 (elbow)
+        s32.moveTimeWrite(pos[7]) #print(pos[7])
+        # Move servo 31 (knee)
+        s31.moveTimeWrite(pos[8]) #print(pos[8])
+        # RIGHT FRONT
+        # Move servo 22 (elbow)
+        s22.moveTimeWrite(pos[4]) #print(pos[4])
+        # Move servo 21 (knee)
+        s21.moveTimeWrite(pos[5]) #print(pos[5])
+        # RIGHT BACK
+        # Move servo 42 (elbow)
+        s42.moveTimeWrite(pos[10]) #print(pos[10])
+        # Move servo 41 (knee)
+        s41.moveTimeWrite(pos[11]) #print(pos[11])
 
-		time.sleep(0.001)
+        time.sleep(0.001)
 
 
 time.sleep(0.1)

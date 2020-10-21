96e705b166bdec35ae55bbb17cc090731607cbcd
polbebe <pb2750@columbia.edu>
2020-10-21 12:39:35 -0400

    RPY update

--- SenseHat/rpy_test.py
+++ SenseHat/rpy_test.py
@@ -2,8 +2,9 @@ from sense_hat import SenseHat
 sense = SenseHat()
 sense.clear()
 
-o = sense.get_orientation()
-pitch = o["pitch"]
-roll = o["roll"]
-yaw = o["yaw"]
-print("pitch {0} roll {1} yaw {2}".format(pitch, roll, yaw))
+while True:
+	o = sense.get_orientation()
+	pitch = o["pitch"]
+	roll = o["roll"]
+	yaw = o["yaw"]
+	print("pitch {0} roll {1} yaw {2}".format(pitch, roll, yaw))

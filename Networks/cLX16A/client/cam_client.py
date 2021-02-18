d2097def153339e85811e28efc2e2c781f4226dd
polbebe <pb2750@columbia.edu>
2021-02-18 10:59:25 -0500

    Cam client v1 server

--- Networks/cLX16A/client/cam_client.py
+++ Networks/cLX16A/client/cam_client.py
@@ -1,39 +1,56 @@
-# Detect Apriltag fiducials in Raspbery Pi camera image
-# From iosoft.blog
- 
+# Detect Apriltag fiducials in Raspbery Pi camera image by iosoft.blog
 import cv2
 from apriltag import apriltag
- 
-TITLE      = "apriltag_view"  # Window title
-TAG        = "tag36h11"        # Tag family, tag16h5, tag36h11
-MIN_MARGIN = 10               # Filter value for tag detection
-FONT       = cv2.FONT_HERSHEY_SIMPLEX  # Font for ID value
-RED        = 0,0,255          # Colour of ident & frame (BGR)
- 
+
+import socket
+
+import time
+
+class CamData():
+    # Constructor method
+    def __init__(self, tag):
+        
+        self.TAG = tag
+        self.MIN_MARGIN = 10
+
+        self.k = 0
+        self.xstart = 0
+        self.ystart = 0
+        self.currentx = 0
+        self.currenty = 0
+
+        self.cam = cv2.VideoCapture(0)
+        self.detector = apriltag(TAG)
+
+        self.ret, self.img = cam.read()
+        self.greys = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        self.det = detector.detect(greys)
+        self.rect = 0
+
+
+    # Read next position in camera
+    def read(self):
+
+        if self.det["margin"] >= self.MIN_MARGIN:
+
+            self.rect = self.det["lb-rb-rt-lt"].astype(int).reshape((-1,1,2))
+
+            if self.k == 0:
+                self.xstart = self.det["center"][0]
+                self.ystart = self.det["center"][1]
+                print('Start pos, x: {}, y: {}'.format(self.xstart, self.ystart))
+
+            self.currentx = det["center"][0] - self.xstart
+            self.currenty = det["center"][1] - self.ystart
+
+            self.k += 1
+
+            return [self.currentx, self.currenty]
+
 if __name__ == '__main__':
-    cam = cv2.VideoCapture(0)
-    detector = apriltag(TAG)
-    k = 0
-    xstart = 0
-    ystart = 0
-    while cv2.waitKey(1) != 0x1b:
-        ret, img = cam.read()
-        greys = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        dets = detector.detect(greys)
-        for det in dets:
-            if det["margin"] >= MIN_MARGIN:
-                rect = det["lb-rb-rt-lt"].astype(int).reshape((-1,1,2))
-                #cv2.polylines(img, [rect], True, RED, 2)
-                ident = str(det["id"])
-                pos = det["center"].astype(int) + (-10,10)
-                #cv2.putText(img, ident, tuple(pos), FONT, 1, RED, 2)
-                if k == 0:
-                    xstart = det["center"][0]
-                    ystart = det["center"][1]
-                    print('Start pos, x: {}, y: {}'.format(xstart, ystart))
-                deltax = det["center"][0] - xstart
-                deltay = det["center"][1] - ystart
-                print("Tag %s: x: %6.1f, y: %6.1f" % (det["id"], deltax, deltay))
-                k = k+1
-        #cv2.imshow(TITLE, img)
-    #cv2.destroyAllWindows()
+    # Construct Cam object and allow use of methods
+    i = CamData("tag36h11")
+
+    while True:
+        pos = i.read()
+        print('x:{}, y:{}'.format(pos[0], pos[1]))

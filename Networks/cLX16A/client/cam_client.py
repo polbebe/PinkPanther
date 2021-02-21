762cd6090af89e8caa282f80840761a846063ac2
polbebe <pb2750@columbia.edu>
2021-02-21 11:00:08 -0500

    cam_client v3.4 pp

--- Networks/cLX16A/client/cam_client.py
+++ Networks/cLX16A/client/cam_client.py
@@ -26,29 +26,29 @@ class CamData():
     def frame(self):
         #cam = cv2.VideoCapture(0)
         #detector = apriltag(self.TAG)
-        while True:
-            ret, img = self.cam.read()
-            greys = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            dets = self.detector.detect(greys)
-            for det in dets:
-                if det["margin"] >= self.MIN_MARGIN:
-                    rect = det["lb-rb-rt-lt"].astype(int).reshape((-1,1,2))
-                    cv2.polylines(img, [rect], True, self.RED, 2)
-                    ident = str(det["id"])
-                    pos = det["center"].astype(int) + (-10,10)
-                    cv2.putText(img, ident, tuple(pos), self.FONT, 1, self.RED, 2)
-                    if self.k == 0:
-                        self.x0 = det["center"][0]
-                        self.y0 = det["center"][1]
-                        print('Start pos, x: {}, y: {}'.format(self.x0, self.y0))
-                    deltax = det["center"][0] - self.x0
-                    deltay = det["center"][1] - self.y0
-                    print("Tag %s: x: %6.1f, y: %6.1f" % (det["id"], deltax, deltay))
-                    self.k += 1
-            cv2.imshow(self.TITLE, img)
-        cv2.destroyAllWindows()
+        #while True:
+        ret, img = self.cam.read()
+        greys = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        dets = self.detector.detect(greys)
+        for det in dets:
+            if det["margin"] >= self.MIN_MARGIN:
+                rect = det["lb-rb-rt-lt"].astype(int).reshape((-1,1,2))
+                #cv2.polylines(img, [rect], True, self.RED, 2)
+                #ident = str(det["id"])
+                pos = det["center"].astype(int) + (-10,10)
+                #cv2.putText(img, ident, tuple(pos), self.FONT, 1, self.RED, 2)
+                if self.k == 0:
+                    self.x0 = det["center"][0]
+                    self.y0 = det["center"][1]
+                    print('Start pos, x: {}, y: {}'.format(self.x0, self.y0))
+                deltax = det["center"][0] - self.x0
+                deltay = det["center"][1] - self.y0
+                print("Tag %s: x: %6.1f, y: %6.1f" % (det["id"], deltax, deltay))
+                self.k += 1
+        #cv2.imshow(self.TITLE, img)
+    #cv2.destroyAllWindows()
 
 if __name__=='__main__':
     i = CamData()
-
-    i.frame()
+    while True:
+        i.frame()

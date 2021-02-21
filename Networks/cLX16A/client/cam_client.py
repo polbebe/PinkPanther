1a708ecb5415f180e4be6bc3cc86cbca8f171f01
polbebe <pb2750@columbia.edu>
2021-02-21 10:33:02 -0500

    cam_client v2 server

--- Networks/cLX16A/client/cam_client.py
+++ Networks/cLX16A/client/cam_client.py
@@ -12,45 +12,53 @@ class CamData():
         
         self.TAG = "tag36h11"
         self.MIN_MARGIN = 10
-
-        self.k = 0
-        self.xstart = 0
-        self.ystart = 0
-        self.currentx = 0
-        self.currenty = 0
+        
+        #
+        self.TITLE = "apriltag_view"
+        self.FONT = cv2.FONT_HERSHEY_SIMPLEX
+        self.RED = 0,0,255
+        #
 
         self.cam = cv2.VideoCapture(0)
         self.detector = apriltag(self.TAG)
 
+        self.k = 0
+        self.x0 = 0
+        self.y0 = 0
+
         self.ret, self.img = self.cam.read()
         self.greys = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
         self.dets = self.detector.detect(self.greys)
-        self.rect = 0
-
 
     # Read next position in camera
     def read(self):
+
         for det in self.dets:
             if det["margin"] >= self.MIN_MARGIN:
 
-                self.rect = det["lb-rb-rt-lt"].astype(int).reshape((-1,1,2))
+                rect = det["lb-rb-rt-lt"].astype(int).reshape((-1,1,2))
+                cv2.polylines(self.img, [rect], True, RED, 2)
+                ident = str(det["id"])
+                pos = det["center"].astype(int) + (-10,10)
+                cv2.putText(self.img, ident, tuple(pos), self.FONT, 1, self.RED, 2)
 
                 if self.k == 0:
-                    self.xstart = det["center"][0]
-                    self.ystart = det["center"][1]
-                    print('Start pos, x: {}, y: {}'.format(self.xstart, self.ystart))
+                    self.x0 = det["center"][0]
+                    self.y0 = det["center"][1]
+                    print('Start pos, x: {}, y: {}'.format(self.x0, self.y0))
 
-                self.currentx = det["center"][0] - self.xstart
-                self.currenty = det["center"][1] - self.ystart
+                deltax = det["center"][0] - self.x0
+                deltay = det["center"][1] - self.y0
 
                 self.k += 1
 
-                return [self.currentx, self.currenty]
+                return [deltax, deltay]
 
 if __name__ == '__main__':
     # Construct Cam object and allow use of methods
     i = CamData()
+    print('Started...')
 
     while True:
         pos = i.read()
-        #print(pos)
+        print(pos)

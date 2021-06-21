bea9a4dde967f019e325becfaabfcfd13b3d8bdf
Pol Bernat <polbernat@MBP-de-Pol-109.lan>
2021-06-21 13:25:54 -0400

    Voltage tester

--- lx16a_ctypes2.0/Voltage.py
+++ lx16a_ctypes2.0/Voltage.py
@@ -24,4 +24,8 @@ for j in range(1,5):
 			motor.setPositionOffset(i,offsets[h])
 		h+=1
 
-print(motor.getVoltage(10))
+for j in range(1,5):
+	u = 10*j
+	r = range(u, u+3)
+	for i in r:
+		print(motor.getVoltage(i))

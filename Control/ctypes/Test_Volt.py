a88c605c05c405d6396942c467deba0d5bc4a7c2
Pol Bernat <polbernat@MBP-de-Pol-109.lan>
2021-06-22 11:08:21 -0400

    Boot check - Unit tests started

---
+++ Control/ctypes/Test_Volt.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from ServoMotor import *
+import time
+
+
+filename = "/dev/ttyUSB0"
+motor = ServoMotor(filename)
+
+# Initialize USB Port
+IO = motor.IO_Init()
+if IO < 0:
+	print('IO exit')
+	sys.exit()
+
+offsets = [30, 0, 64, 0, 70, 50, 26, 0, 55, 80, 90, 35]
+h = 0
+# Set servo mode to all servos with their offset
+for j in range(1,5):
+	u = 10*j
+	r = range(u, u+3)
+	for i in r:
+		motor.setServoMode(i)
+		if offsets[h]!=0:
+			motor.setPositionOffset(i,offsets[h])
+		h+=1
+
+for j in range(1,5):
+	u = 10*j
+	r = range(u, u+3)
+	for i in r:
+		print("Motor {}: {} mV ?".format(i, motor.getVoltage(i)))

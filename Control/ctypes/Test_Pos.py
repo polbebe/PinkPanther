4cf041aae8b078606c5860222c9cb5362762613e
Pol Bernat <polbernat@MBP-de-Pol-109.lan>
2021-06-22 11:57:45 -0400

    Positions unit test mean error

--- Control/ctypes/Test_Pos.py
+++ Control/ctypes/Test_Pos.py
@@ -2,9 +2,10 @@
 from ServoMotor import *
 import time
 
-
 filename = "/dev/ttyUSB0"
 motor = ServoMotor(filename)
+
+cumError = []
 
 # Initialize USB Port
 IO = motor.IO_Init()
@@ -24,51 +25,72 @@ for j in range(1,5):
 			motor.setPositionOffset(i,offsets[h])
 		h+=1
 
-pos = [510, 750, 583, 500, 250, 417, 500, 750, 583, 500, 250, 417]
-h = 0
-for j in range(1,5):
-	u = 10*j
-	r = range(u, u+3)
-	for i in r:
-		if h>5:
-			motor.move(i, int(pos[h]), 700)
-		else:
+for i in range(3):
+	pos = [510, 750, 583, 500, 250, 417, 500, 750, 583, 500, 250, 417]
+	h = 0
+	for j in range(1,5):
+		u = 10*j
+		r = range(u, u+3)
+		for i in r:
+			if h>5:
+				motor.move(i, int(pos[h]), 700)
+			else:
+				motor.move(i, int(pos[h]), 1000)
+			h+=1
+
+	time.sleep(3)
+
+	error = []
+
+	h = 0
+	for j in range(1,5):
+		u = 10*j
+		r = range(u, u+3)
+		for i in r:
+			print("Motor {}: {} Pos Error".format(i, abs(pos[h] - motor.readPosition(i))))
+			error.append(abs(pos[h] - motor.readPosition(i)))
+			h+=1
+
+	cumError.append(error)
+	
+	print("-----------------")
+
+	time.sleep(1)
+
+	pos = [500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500]
+	h = 0
+	for j in range(1,5):
+		u = 10*j
+		r = range(u, u+3)
+		for i in r:
 			motor.move(i, int(pos[h]), 1000)
-		h+=1
+			h+=1
 
-time.sleep(3)
+	time.sleep(3)
 
-h = 0
-for j in range(1,5):
-	u = 10*j
-	r = range(u, u+3)
-	for i in r:
-		print("Motor {}: {} Pos Error".format(i, abs(pos[h] - motor.readPosition(i))))
-		h+=1
+	error = []
 
-print("-----------------")
+	h = 0
+	for j in range(1,5):
+		u = 10*j
+		r = range(u, u+3)
+		for i in r:
+			print("Motor {}: {} Pos Error".format(i, abs(pos[h] - motor.readPosition(i))))
+			error.append(abs(pos[h] - motor.readPosition(i)))
+			h+=1
 
-time.sleep(1)
+	cumError.append(error)
 
-pos = [500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500]
-h = 0
-for j in range(1,5):
-	u = 10*j
-	r = range(u, u+3)
-	for i in r:
-		motor.move(i, int(pos[h]), 1000)
-		h+=1
+	print("-----------------")
 
-time.sleep(3)
+	time.sleep(1)
 
-h = 0
-for j in range(1,5):
-	u = 10*j
-	r = range(u, u+3)
-	for i in r:
-		print("Motor {}: {} Pos Error".format(i, abs(pos[h] - motor.readPosition(i))))
-		h+=1
+# For each column
+for i in range(cumError.shape[1]):
+	mean = 0
+	# For each row
+	for j in range(cumError.shape[2]):
+		mean += cumError[j, i]
+	mean = mean/cumError.shape[1]
+	print(mean)
 
-print("-----------------")
-
-time.sleep(1)

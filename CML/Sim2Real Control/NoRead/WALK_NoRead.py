f650483ebd2da907993b8f161aa6acdba7d4bc84
Pol Bernat <polbernat@MBP-de-Pol-109.lan>
2021-07-31 17:10:26 -0400

    Front & Back move sync

--- CML/Sim2Real Control/NoRead/WALK_NoRead.py
+++ CML/Sim2Real Control/NoRead/WALK_NoRead.py
@@ -25,9 +25,9 @@ def convFns(pos, convType):
 		else:
 			targ[i] = conv[i](pos[i], convType)
 	return targ
-
+'''
 # Return target position
-def act(t, a, b, c, d, e):
+def act_shoulders&armpits(t, a, b, c, d, e):
 	# Calculate desired position
 	desired_p = np.zeros(12)
 	# Positive
@@ -51,11 +51,41 @@ def act(t, a, b, c, d, e):
 
 	# Return desired new position
 	return convFns(desired_p, "sim2real")
+'''
+
+# Front and back legs diff
+# Return target position
+def act(t, a, b, c, d, e):
+	# Calculate desired position
+	desired_p = np.zeros(12)
+
+	pos_front_v = a * np.sin(t * e) + b
+	neg_front_v = -a * np.sin(t * e) + b
+	pos_back_v = c * np.sin(t * e) + d
+	neg_back_v = -c * np.sin(t * e) + d
+
+	front_pos = [1, 2]
+	front_neg = [4, 5]
+	back_pos = [10, 11]
+	back_neg [7, 8]
+
+	zero = [0, 3, 6, 9]
+
+	# Assign	
+	desired_p[front_pos] = pos_front_v
+	desired_p[front_neg] = neg_front_v
+	desired_p[back_pos] = pos_back_v
+	desired_p[back_neg] = neg_back_v
+	desired_p[zero] = 0
+
+	# Return desired new position
+	return convFns(desired_p, "sim2real")
+
 
 # Return position to take
 def get_action(steps):
 	#params = np.array([0.15, -0.3, 0.2, 0.3, 0.2]) # From Hardcoded_0.2.0
-	params = np.array([0.15, 0.0, 0.2, 0.25, 0.2]) # From Hardcoded_0.2.0
+	params = np.array([0.2, 0.0, 0.15, 0.0, 0.2]) # From Hardcoded_0.2.0
 	return act(steps, *params)
 
 # MOVE MOTOR TO GIVEN POSITION

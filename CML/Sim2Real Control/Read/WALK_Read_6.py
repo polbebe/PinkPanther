9de033511c0e0f58bd46e5bf2272ea39e1ef1f2b
polbebe <pb2750@columbia.edu>
2022-02-18 12:20:49 -0500

    See if reading works

--- CML/Sim2Real Control/Read/WALK_Read_6.py
+++ CML/Sim2Real Control/Read/WALK_Read_6.py
@@ -54,8 +54,8 @@ def act(t, a, b, c, d, e, f, obs):
 
 # Return position to take
 def get_action(steps, obs):
-	#params = np.array(np.load('params/HillClimber/23_01_2022/best_overall.npy'))
-	params = np.array([ 0.31607133, -0.04617572, -0.25435251,  0.09736614, -8.81590009,  6.12591908]) # 12_02_2022 params trained on envs auto-tuned to be close to the env I manually tuned
+	params = np.array(np.load('params/HillClimber/23_01_2022/best_overall.npy'))
+	#params = np.array([ 0.31607133, -0.04617572, -0.25435251,  0.09736614, -8.81590009,  6.12591908]) # 12_02_2022 params trained on envs auto-tuned to be close to the env I manually tuned
 	#params = np.array([-0.16476964, 0.02548534, 0.16893791, 0.09441782, 9.44620473, -6.1950588]) # 27_01_2022 params trained on envs auto-tuned to be close to the env I manually tuned
 	#params = np.array([ 0.22853782, 0.06146434, 0.25060128, 0.09051928, 10.81942692, 2.98455422]) # 31_01_2022 params trained on envs auto-tuned to be close to the env I manually tuned
 	#params[4]-=4
@@ -90,6 +90,9 @@ def walk(pos):
 			real_pos.append(motor.readPosition(i))
 			h+=1
 		time.sleep(0.005)
+		print(pos)
+		print(real_pos)
+		print()
 	return real_pos
 
 # Initialize motors as servos and set offset
